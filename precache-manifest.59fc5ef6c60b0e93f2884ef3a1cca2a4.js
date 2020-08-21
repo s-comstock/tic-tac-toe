01b73c98ade4a40639b26473add3de3fa96c7b72
Sam Comstock <31906344+s-comstock@users.noreply.github.com>
2020-08-20 18:44:22 -0600

    Updates

---
+++ precache-manifest.59fc5ef6c60b0e93f2884ef3a1cca2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef116dfb3e7525fb1d6cd4e383274aa",
+    "url": "/s-comstock/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "df563247dc6754c07759",
+    "url": "/s-comstock/tic-tac-toe/static/css/main.3e11c52d.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/s-comstock/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/s-comstock/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df563247dc6754c07759",
+    "url": "/s-comstock/tic-tac-toe/static/js/main.5820f438.chunk.js"
+  },
+  {
+    "revision": "3046349b68b21e4b8fef",
+    "url": "/s-comstock/tic-tac-toe/static/js/runtime-main.cb8a3a97.js"
+  }
+]);
